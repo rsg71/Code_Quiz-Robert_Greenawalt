dc2a1f86c0165692696710dc7137717098fe4a29
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-09-09 22:31:19 -0400

    game functions properly, and added styling for highscores

--- highscorescript.js
+++ highscorescript.js
@@ -1,5 +1,5 @@
 var highScoreScreen = document.getElementById("highscore-screen")
-var goBack = document.getElementById("go-back");
+var goBack = document.querySelector(".go-back");
 var clearHighscores = document.getElementById("clear-highscores");
 var userHighScore = document.getElementById("user-high-score");
 
@@ -8,18 +8,17 @@ function renderHighScore () {
     userHighScoreInitials = localStorage.getItem("initials");
     userHighScoreNumber = localStorage.getItem("score");
     
-    userHighScore.textContent = userHighScoreInitials + " - " + userHighScoreNumber;
+    if(userHighScoreInitials == null || userHighScoreNumber == null) {
+        userHighScoreInitials.textContent = " ";
+        userHighScoreNumber.textContent = " ";
+    }
+
+    userHighScore.textContent = userHighScoreInitials + " : " + userHighScoreNumber;
     
 } 
 
 renderHighScore();
 
-
-// goBack.addEventListener("click", function(event) {
-//     location.reload();
-
-// ------------------------- need a way to get back to quiz game instead of location.reload ();--------------------
-// });
 
 clearHighscores.addEventListener("click", function(event) {
     localStorage.clear();
--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var startBtn = document.querySelector("#start-button");
 var quiz = document.getElementById("quiz");
 
 startBtn.addEventListener("click", initialize);
-var allDoneScreen = document.getElementById("all-done-screen");
-allDoneScreen.style.display = "none";
+var gameOverScreen = document.getElementById("game-over-screen");
+gameOverScreen.style.display = "none";
 
 
 
@@ -18,55 +18,55 @@ var score = 0;
 var scoreAmount = document.getElementById("score");
 var count = 100;
 
-//create questions
+//list of questions
 var questions = [
     {
-        question: "What is the capital of Brazil?",
-        choiceA: "Sao Paolo",
-        choiceB: "Correct",
-        choiceC: "Rio de Janeiro",
-        choiceD: "Recife",
-        correct: "B"
-    },
-    {
-        question: "Who is the tallest person ever?",
-        choiceA: "Andre the Giant",
-        choiceB: "Don Koehler",
-        choiceC: "Xi Shun",
-        choiceD: "Correct",
-        correct: "D"
-    },
-    {
-        question: "When was the lightbulb invented?",
-        choiceA: "1879",
-        choiceB: "1844",
-        choiceC: "1867",
-        choiceD: "1858",
-        correct: "A"
-    },
-    {
-        question: "How many eyes does a spider have?",
-        choiceA: "Four",
-        choiceB: "Two",
-        choiceC: "Correct",
-        choiceD: "Six",
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        choiceA: "javascript",
+        choiceB: "scripted",
+        choiceC: "script",
+        choiceD: "js",
         correct: "C"
     },
     {
-        question: "What building is on the back of the $100 dollar bill?",
-        choiceA: "Correct",
-        choiceB: "United States Capitol",
-        choiceC: "Supreme Court",
-        choiceD: "Lincoln Memorial",
+        question: "Choose the correct JavaScript syntax to change the content of the following HTML code:",
+        choiceA: "document.getElement(“geek”).innerHTML=”I am a Geek”;",
+        choiceB: "document.getElementById(“geek”).innerHTML=”I am a Geek”;",
+        choiceC: "document.getId(“geek”)=”I am a Geek”;",
+        choiceD: "document.getElementById(“geek”).innerHTML=I am a Geek;",
+        correct: "B"
+    },
+    {
+        question: "Which will display “GeeksforGeeks” in an alert box using JavaScript?",
+        choiceA: "alertbox(“GeeksforGeeks”);",
+        choiceB: "msg(“GeeksforGeeks”);",
+        choiceC: "msgbox(“GeeksforGeeks”);",
+        choiceD: "alert(“GeeksforGeeks”);",
+        correct: "D"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called “geek.js”?",
+        choiceA: "script src=”geek.js”",
+        choiceB: "script href=”geek.js",
+        choiceC: "script ref=”geek.js”",
+        choiceD: "script name=”geek.js”",
         correct: "A"
     },
     {
-        question: "What does LASER stand for?",
-        choiceA: "Light Amplified by Sending Emitted Rays",
-        choiceB: "Light Aggregated Self Emitting Radiowaves",
-        choiceC: "Light Amplified by Stimulated Emission of Rays",
-        choiceD: "Correct",
-        correct: "D"
+        question: "Which of the following is not a reserved word in JavaScript?",
+        choiceA: "interface",
+        choiceB: "throws",
+        choiceC: "program",
+        choiceD: "short",
+        correct: "C"
+    },
+    {
+        question: "Which built-in method returns the character at the specified index?",
+        choiceA: "characterAt()",
+        choiceB: "getCharAt()",
+        choiceC: "charAt()",
+        choiceD: "None of the above",
+        correct: "C"
     },
 
 
@@ -80,7 +80,6 @@ var runningQuestion = 0;
 
 //changing display from start game button to actual game
 function initialize () {
-    console.log("started")
     startScreen.style.display = "none";
     startBtn.style.display = "none";
     quiz.classList.remove("hide");
@@ -89,11 +88,15 @@ function initialize () {
     //counter
     intId = setInterval(countdown, 1000);
     var counter = document.querySelector("#time_remaining");
-    // var count = 100;
 
     function countdown() {
         count--;
         counter.textContent = "Time remaining: " + count;
+        if(count === 0) {
+            clearInterval(intId);
+            GameOverScreen();
+        }
+        
     }
 
     renderQuestion ();
@@ -112,73 +115,112 @@ function renderQuestion () {
     choiceD.innerHTML = q.choiceD ;
 }
 
+////declaring the game over screen for the first time, so that it can be accessed if the counter hits 0
+function GameOverScreen () {
+    
+            document.getElementById("quiz").className = "hide";
+            gameOverScreen.classList.remove("hide");
+            gameOverScreen.style.display = "block";
+
+            var finalScore = document.getElementById("final-score");
+            finalScore.textContent = "Your final score is " + score + " / 6";
+            // 
+
+            var submit = document.getElementById("submit");
+            var initials = document.getElementById("initials");
+            var highScoreScreen = document.getElementById("highscore-screen")
+            var userHighScore = document.getElementById("user-high-score")
+    
+
+            submit.addEventListener("click", function(event) {
+                event.preventDefault();
+                    
+                localStorage.setItem("initials", initials.value);
+                localStorage.setItem("score", score);
+            
+                gameOverScreen.style.display = "none";
+                
+                highScoreScreen.classList.remove("hide");
+    
+                userHighScore.textContent = initials.value + " : " + score;
+                
+            
+            });
+            var clearHighscores = document.getElementById("clear-highscores");
+            clearHighscores.addEventListener("click", function(event) {
+                localStorage.clear();
+            });
+            
+            var goBack = document.querySelector(".go-back");
+            goBack.addEventListener("click", function () {
+                startScreen.style.display = "block";
+                startBtn.style.display = "block";
+                quiz.style.display = "none";
+                highScoreScreen.style.display = "none";
+    
+    
+    
+                location.reload()
+            });
+            
+        }
+// Game over screen ends here
 
 
 function checkAnswer (answer) {
+    // answerIsCorrect
     if ( answer == questions[runningQuestion].correct) {
         score ++;
         console.log(" score is " + score);
         scoreAmount.textContent = "Score: " + score + "/6";
 
-        // answerIsCorrect
+    // answerIsWrong;
     } else {
-        // answerIsWrong;
-        
         count = count - 10;
-        console.log("anwer is wrong")
     }
     if (runningQuestion < lastQuestion) {
         runningQuestion ++;
         renderQuestion();
     }
     else {
-        console.log("endgame");
         clearInterval(intId);
-
-
-        function showHighscoresScreen () {
-    
-            document.getElementById("quiz").className = "hide";
-            allDoneScreen.classList.remove("hide");
-            allDoneScreen.style.display = "block";
-
-            var finalScore = document.getElementById("final-score");
-            finalScore.textContent = "Your final score is " + score + " / 6";
-        }
         
-        showHighscoresScreen();
-        console.log("getting there")
+        GameOverScreen();
 
         // ----------------------------------------------------------------------------------------------------------
-        //after submit button
+        //below is what occurs after submit button is clicked
         var submit = document.getElementById("submit");
         var initials = document.getElementById("initials");
         var highScoreScreen = document.getElementById("highscore-screen")
         var userHighScore = document.getElementById("user-high-score")
 
 
+        //submit button and the events that follow:
         submit.addEventListener("click", function(event) {
             event.preventDefault();
             
-                
+            //stores the values of initials and score
             localStorage.setItem("initials", initials.value);
             localStorage.setItem("score", score);
         
-            allDoneScreen.style.display = "none";
-            
+
+            //shows the relevant screen (the high score screen)
+            gameOverScreen.style.display = "none";
             highScoreScreen.classList.remove("hide");
 
-            userHighScore.textContent = initials.value + " - " + score;
+            userHighScore.textContent = initials.value + " : " + score;
             
         
         });
+
+        //creation of the clear high scores button:
         var clearHighscores = document.getElementById("clear-highscores");
         clearHighscores.addEventListener("click", function(event) {
             localStorage.clear();
         });
-        console.log("up here")
         
-        var goBack = document.querySelector("#go-back");
+        //creation of the go back button:
+        var goBack = document.querySelector(".go-back");
         goBack.addEventListener("click", function () {
             startScreen.style.display = "block";
             startBtn.style.display = "block";
